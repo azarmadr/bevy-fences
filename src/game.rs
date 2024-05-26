22af20fed28f75ef653391d8a42328a84bca2eac
Asif <azarshaik454@gmail.com>
2024-05-26 19:50:48 +0530

    Board setup

--- src/game.rs
+++ src/game.rs
@@ -1,22 +1,146 @@
-use bevy::prelude::*;
+use std::cmp;
 
-#[derive(Resource)]
-pub struct Board(pub fences::Board);
+use bevy::{prelude::*, sprite::Anchor};
+use bevy_prototype_lyon::{prelude::*, shapes};
+use fences::{solver::Idx, BoardGeom, FencesSolver};
+
+#[derive(Resource, Deref)]
+pub struct Board(fences::Board);
 
 #[derive(Component)]
-struct Node;
+struct Node(Idx);
 #[derive(Component)]
-struct Edge;
+enum Edge {
+    H(Idx),
+    V(Idx),
+}
 #[derive(Component)]
-struct Cell;
+struct Cell(Idx);
 #[derive(Component)]
 struct Grid;
 
-fn board_setup(mut commands: Commands, board: Res<Board>) {
-    commands.spawn(Grid);
+fn board_setup(mut commands: Commands, board: Res<Board>, windows: Query<&Window>) {
+    let window = windows.single();
+    let window_size = (window.resolution.height(), window.resolution.width());
+
+    let board_size = board.size();
+    let scale =
+        (window_size.0 / board_size.0 as f32).min(window_size.1 / board_size.1 as f32) * 0.8;
+
+    let get_node = |x: usize, y: usize, z: f32| -> Vec3 {
+        Vec3 {
+            x: scale * (x as f32 - board_size.1 as f32 / 2.),
+            y: scale * (y as f32 - board_size.0 as f32 / 2.),
+            z,
+        }
+    };
+    commands
+        .spawn((
+            Grid,
+            ShapeBundle {
+                path: GeometryBuilder::build_as(&shapes::Rectangle {
+                    extents: Vec2 {
+                        x: board_size.1 as f32 * (scale * 1.2),
+                        y: board_size.0 as f32 * (scale * 1.2),
+                    },
+                    origin: RectangleOrigin::Center,
+                }),
+                ..default()
+            },
+            Fill::color(Color::BLACK),
+        ))
+        .with_children(|p| {
+            for x in 0..=board_size.1 {
+                for y in 0..=board_size.0 {
+                    p.spawn((
+                        Node((y, x)),
+                        Name::new(format!("Node({x},{y})")),
+                        ShapeBundle {
+                            path: GeometryBuilder::build_as(&shapes::Circle {
+                                radius: 10.,
+                                ..default()
+                            }),
+                            spatial: SpatialBundle::from_transform(
+                                Transform::default().with_translation(get_node(x, y, 1.)),
+                            ),
+                            ..Default::default()
+                        },
+                        Fill::color(Color::WHITE),
+                    ));
+                }
+            }
+            for (idx, t) in board.0.tasks_iter() {
+                p.spawn((
+                    Cell(idx),
+                    Name::new(format!("Cell({idx:?})")),
+                    ShapeBundle {
+                        path: GeometryBuilder::build_as(&shapes::Rectangle {
+                            extents: Vec2::splat(scale - 10.),
+                            origin: RectangleOrigin::Center,
+                        }),
+                        spatial: SpatialBundle::from_transform(
+                            Transform::default().with_translation(
+                                get_node(idx.1, idx.0, 1.)
+                                    + Vec3 {
+                                        x: scale / 2.,
+                                        y: scale / 2.,
+                                        z: 0.,
+                                    },
+                            ),
+                        ),
+                        ..default()
+                    },
+                    Fill::color(Color::NONE),
+                ))
+                .with_children(|p| {
+                    if let Some(v) = t {
+                        p.spawn(Text2dBundle {
+                            text: Text::from_section(
+                                format!("{v}"),
+                                TextStyle {
+                                    font_size: scale,
+                                    ..default()
+                                },
+                            ),
+                            transform: Transform::from_translation(Vec3::Z * 2.),
+                            text_anchor: Anchor::Center,
+                            ..Default::default()
+                        });
+                    }
+                });
+            }
+            for ((dir, x, y), _) in board.fences_iter() {
+                p.spawn((
+                    Name::new(format!("E({dir}, {y}, {x})")),
+                    if dir == 0 {
+                        Edge::H((x, y))
+                    } else {
+                        Edge::V((x, y))
+                    },
+                    ShapeBundle {
+                        path: GeometryBuilder::build_as(&shapes::Line(
+                            Vec2::default(),
+                            if dir == 0 {
+                                Vec2 { y: 0., x: scale }
+                            } else {
+                                Vec2 { x: 0., y: scale }
+                            },
+                        )),
+                        spatial: SpatialBundle::from_transform(
+                            Transform::default().with_translation(get_node(y, x, 1.)),
+                        ),
+                        ..default()
+                    },
+                    Stroke::new(Color::NONE, 10.0),
+                ));
+            }
+        });
 }
 
-struct BoardPlugin;
+pub struct BoardPlugin;
 impl Plugin for BoardPlugin {
-    fn build()
+    fn build(&self, app: &mut App) {
+        app.insert_resource(Board("3#33 ".parse().unwrap()))
+            .add_systems(Startup, board_setup);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 
+#[cfg(debug_assertions)]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod game;
+
 fn main() {
-    App::new()
-        .insert_resource(Msaa::Sample4)
-        .insert_resource(fences::Board("2#33".parse().unwrap()))
-        .add_plugins(DefaultPlugins)
+    let mut app = App::new();
+    app.insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Fences".to_string(),
+                // Bind to canvas included in `index.html`
+                canvas: Some("#bevy".to_owned()),
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
+                resolution: (480., 720.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(ShapePlugin)
-        .add_systems(Startup, (camera_setup, setup_system))
-        .run();
+        .add_systems(Startup, camera_setup)
+        .add_plugins(game::BoardPlugin);
+
+    #[cfg(debug_assertions)]
+    {
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+    app.run();
 }
 
 fn camera_setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn setup_system(mut commands: Commands) {
+fn _setup_system(mut commands: Commands) {
     let shape = shapes::RegularPolygon {
         sides: 6,
         feature: shapes::RegularPolygonFeature::Radius(200.0),
