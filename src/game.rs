48a31e5a0e8509f940f623dc4e3ae84aa01e60b5
Asif <azarshaik454@gmail.com>
2024-06-01 21:45:39 +0530

    Created update systems to manage board. Game is now functional, with only one puzzle so far

--- src/game.rs
+++ src/game.rs
@@ -1,25 +1,85 @@
-use std::cmp;
-
 use bevy::{prelude::*, sprite::Anchor};
 use bevy_prototype_lyon::{prelude::*, shapes};
-use fences::{solver::Idx, BoardGeom, FencesSolver};
+use fences::{board::Fence, solver::Idx, BoardGeom, FencesSolver};
 
-#[derive(Resource, Deref)]
-pub struct Board(fences::Board);
+pub struct BoardPlugin;
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<BoardAssets>()
+            .insert_resource(Board::new(
+                "3#33    
+0 0 0 y
+1 0 2 y",
+            ))
+            .add_systems(
+                Update,
+                (
+                    edge_interaction_system,
+                    edge_click_system,
+                    board_update_system,
+                ),
+            )
+            .add_systems(Startup, board_setup);
+    }
+}
+
+#[derive(Resource, Deref, DerefMut)]
+pub struct Board(#[deref] fences::Board, usize);
+impl Board {
+    fn new(board_str: &str) -> Self {
+        let mut b = Self(board_str.parse().unwrap(), 0);
+        b.1 = b.0.moves().len();
+        b
+    }
+}
 
 #[derive(Component)]
 struct Node(Idx);
-#[derive(Component)]
+#[derive(Component, Debug)]
 enum Edge {
     H(Idx),
     V(Idx),
+}
+impl Edge {
+    fn play_in(&self, board: &mut Board, v: bool) {
+        match self {
+            Edge::H(idx) => board.play(0, *idx, v, "Game Move".to_string()),
+            Edge::V(idx) => board.play(1, *idx, v, "Game Move".to_string()),
+        }
+    }
 }
 #[derive(Component)]
 struct Cell(Idx);
 #[derive(Component)]
 struct Grid;
 
-fn board_setup(mut commands: Commands, board: Res<Board>, windows: Query<&Window>) {
+#[derive(Resource)]
+struct BoardAssets {
+    edge_colors: (Color, Color, Color),
+}
+impl Default for BoardAssets {
+    fn default() -> Self {
+        Self {
+            edge_colors: (Color::NONE, Color::GREEN, Color::GRAY),
+        }
+    }
+}
+impl BoardAssets {
+    fn edge_color(&self, edge: &Fence) -> Color {
+        match edge.0 {
+            Some(true) => self.edge_colors.1,
+            Some(false) => self.edge_colors.2,
+            None => self.edge_colors.0,
+        }
+    }
+}
+
+fn board_setup(
+    mut commands: Commands,
+    board: Res<Board>,
+    assets: Res<BoardAssets>,
+    windows: Query<&Window>,
+) {
     let window = windows.single();
     let window_size = (window.resolution.height(), window.resolution.width());
 
@@ -30,7 +90,7 @@ fn board_setup(mut commands: Commands, board: Res<Board>, windows: Query<&Window
     let get_node = |x: usize, y: usize, z: f32| -> Vec3 {
         Vec3 {
             x: scale * (x as f32 - board_size.1 as f32 / 2.),
-            y: scale * (y as f32 - board_size.0 as f32 / 2.),
+            y: scale * (board_size.0 as f32 / 2. - y as f32),
             z,
         }
     };
@@ -83,7 +143,7 @@ fn board_setup(mut commands: Commands, board: Res<Board>, windows: Query<&Window
                                 get_node(idx.1, idx.0, 1.)
                                     + Vec3 {
                                         x: scale / 2.,
-                                        y: scale / 2.,
+                                        y: -scale / 2.,
                                         z: 0.,
                                     },
                             ),
@@ -109,38 +169,141 @@ fn board_setup(mut commands: Commands, board: Res<Board>, windows: Query<&Window
                     }
                 });
             }
-            for ((dir, x, y), _) in board.fences_iter() {
+            for ((dir, x, y), v) in board.fences_iter() {
+                let mut origin = Vec2 {
+                    y: 0.,
+                    x: scale / 2.,
+                };
+                let mut extents = Vec2 { x: scale, y: 10. };
+                if dir == 1 {
+                    extents = extents.yx();
+                    origin = origin.yx();
+                    origin.y = -origin.y;
+                }
+                let rect = shapes::Rectangle {
+                    origin: RectangleOrigin::CustomCenter(origin),
+                    extents,
+                };
                 p.spawn((
-                    Name::new(format!("E({dir}, {y}, {x})")),
+                    Name::new(format!("E({dir}, {x}, {y})")),
                     if dir == 0 {
                         Edge::H((x, y))
                     } else {
                         Edge::V((x, y))
                     },
                     ShapeBundle {
-                        path: GeometryBuilder::build_as(&shapes::Line(
-                            Vec2::default(),
-                            if dir == 0 {
-                                Vec2 { y: 0., x: scale }
-                            } else {
-                                Vec2 { x: 0., y: scale }
-                            },
-                        )),
+                        path: GeometryBuilder::build_as(&rect),
                         spatial: SpatialBundle::from_transform(
                             Transform::default().with_translation(get_node(y, x, 1.)),
                         ),
                         ..default()
                     },
-                    Stroke::new(Color::NONE, 10.0),
+                    Fill::color(assets.edge_color(v)),
+                    Stroke::new(Color::NONE, 3.),
                 ));
             }
         });
 }
 
-pub struct BoardPlugin;
-impl Plugin for BoardPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(Board("3#33 ".parse().unwrap()))
-            .add_systems(Startup, board_setup);
+#[derive(Component)]
+struct EdgeSelected;
+
+fn edge_interaction_system(
+    _board: Res<Board>,
+    mut commands: Commands,
+    mut mouse_events: EventReader<CursorMoved>,
+    q_camera: Query<(&Camera, &GlobalTransform)>,
+    mut edges: Query<(Entity, &mut Stroke, &GlobalTransform, &Edge, &Path)>,
+    hovered_edges: Query<Entity, With<EdgeSelected>>,
+) {
+    let (camera, camera_transform) = q_camera.single();
+
+    for event in mouse_events.read() {
+        hovered_edges.iter().for_each(|e| {
+            commands.entity(e).remove::<EdgeSelected>();
+        });
+        if let Some(pos) = camera.viewport_to_world_2d(camera_transform, event.position) {
+            // println!("{pos}");
+            let mut found = false;
+            edges.iter_mut().for_each(|(entity, mut s, t, e, _p)| {
+                let diff = pos - t.translation().xy();
+                let scale = t.to_scale_rotation_translation().0;
+                s.color = if !found
+                    && match e {
+                        Edge::H(_) => {
+                            diff.x > 0. && diff.x < 80. * scale.x && diff.y.abs() < 10. * scale.y
+                        }
+                        Edge::V(_) => {
+                            diff.y < 0. && diff.y > -80. * scale.y && diff.x.abs() < 10. * scale.x
+                        }
+                    } {
+                    // println!("{:?} -> {:?} -> {:?}", e, diff, p.0);
+                    found = true;
+                    commands.entity(entity).insert(EdgeSelected);
+                    Color::BLUE
+                } else {
+                    Color::NONE
+                }
+            })
+        }
+    }
+}
+
+fn edge_click_system(
+    mut board: ResMut<Board>,
+    hovered_edge: Query<&Edge, With<EdgeSelected>>,
+    mouse_button: Res<ButtonInput<MouseButton>>,
+) {
+    let v = if mouse_button.just_pressed(MouseButton::Left) {
+        true
+    } else if mouse_button.just_pressed(MouseButton::Right) {
+        false
+    } else {
+        return;
+    };
+    if let Ok(edge) = hovered_edge.get_single() {
+        edge.play_in(&mut board, v);
+        println!("{}", board.0);
+    }
+}
+
+impl Board {
+    fn is_updated(&self) -> bool {
+        self.1 != self.0.moves().len()
+    }
+}
+fn board_update_system(
+    board: Res<Board>,
+    mut edges: Query<(&mut Fill, &Edge)>,
+    assets: Res<BoardAssets>,
+) {
+    if !board.is_changed() || !board.is_updated() {
+        return;
+    }
+    for mv in (board.1)..board.0.moves().len() {
+        let fences::board::Move {
+            direction,
+            idx,
+            value,
+            ..
+        } = board.moves()[mv];
+        let mut fill = edges
+            .iter_mut()
+            .find_map(|(f, e)| {
+                if match e {
+                    Edge::H(g_idx) => direction == 0 && idx == *g_idx,
+                    Edge::V(g_idx) => direction == 1 && idx == *g_idx,
+                } {
+                    Some(f)
+                } else {
+                    None
+                }
+            })
+            .unwrap();
+        fill.color = if value {
+            assets.edge_colors.1
+        } else {
+            assets.edge_colors.2
+        }
     }
 }
